dcc0d58654b3c3dd131ee283ed03e93d72f49c5f
Maas Lalani <maaslalani0@gmail.com>
2021-11-02 21:30:15 -0400

    We don't really need cobra

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"github.com/maaslalani/confetty/cmd"
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/maaslalani/confetty/confetti"
+	"github.com/maaslalani/confetty/fireworks"
 )
 
 func main() {
-	cmd.Execute()
+	var err error
+	if len(os.Args) > 1 && os.Args[1] == "fireworks" {
+		p := tea.NewProgram(fireworks.InitialModel(), tea.WithAltScreen())
+		err = p.Start()
+	} else {
+		p := tea.NewProgram(confetti.InitialModel(), tea.WithAltScreen())
+		err = p.Start()
+	}
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }

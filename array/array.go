c3f294bd48c6002bc5e2c919a38080fb91f6fea7
Maas Lalani <maas@lalani.dev>
2022-04-16 14:20:15 -0400

    feat: generic array sampling

--- array/array.go
+++ array/array.go
@@ -2,8 +2,7 @@ package array
 
 import "math/rand"
 
-// Sample returns a random element from an array
-// (can't wait for generics!)
-func Sample(s []string) string {
-	return s[rand.Intn(len(s))]
+// Sample returns a random element from a generic array
+func Sample[T any](arr []T) T {
+	return arr[rand.Intn(len(arr))]
 }
